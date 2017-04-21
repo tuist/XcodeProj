320703fe365a5abeacb82135b363d76810f7b672
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-04-21 16:47:28 +0200

    Create Fixtures iOS project

---
+++ Fixtures/iOS/iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Pedro Pinera Buendia on 21.04.17.
+//  Copyright © 2017 es.ppinera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Fixtures/iOS/iOSTests/iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSTests.swift
+//  iOSTests
+//
+//  Created by Pedro Pinera Buendia on 21.04.17.
+//  Copyright © 2017 es.ppinera. All rights reserved.
+//
+
+import XCTest
+@testable import iOS
+
+class iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
