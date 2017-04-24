631bd527b3632e39dac986ea15f2e5ddee076567
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-04-24 18:18:17 +0200

    Add XCBuildConfiguration and tests

---
+++ Sources/XCBuildConfiguration.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+// This is the element for listing build configurations.
+public struct XCBuildConfiguration: Isa {
+   
+    // MARK: - Attributes
+    
+    let reference: String
+    
+    public let isa: String = "XCBuildConfiguration"
+    
+    /// The path to a xcconfig file
+    let baseConfigurationReference: String?
+    
+    /// A map of build settings.
+    let buildSettings: Dictionary<String, String>
+    
+    /// The configuration name.
+    let name: String
+    
+    // MARK: - Init
+    
+    public init(reference: String,
+                baseConfigurationReference: String? = nil,
+                buildSettings: Dictionary<String, String>,
+                name: String) {
+        self.reference = reference
+        self.baseConfigurationReference = baseConfigurationReference
+        self.buildSettings = buildSettings
+        self.name = name
+    }
+    
+    // MARK: - Public
+    
+    public func addingBuild(setting: String, value: String) -> XCBuildConfiguration {
+        var mutableSettings = self.buildSettings
+        mutableSettings[setting] = value
+        return XCBuildConfiguration(reference: self.reference,
+                                    baseConfigurationReference: self.baseConfigurationReference,
+                                    buildSettings: mutableSettings,
+                                    name: self.name)
+    }
+    
+    public func removingBuild(setting: String) -> XCBuildConfiguration {
+        var mutableSettings = self.buildSettings
+        mutableSettings.removeValue(forKey: setting)
+        return XCBuildConfiguration(reference: self.reference,
+                                    baseConfigurationReference: self.baseConfigurationReference,
+                                    buildSettings: mutableSettings,
+                                    name: self.name)
+    }
+}
---
+++ Tests/xcodeprojTests/XCBuildConfigurationSpec.swift
@@ -0,0 +1,27 @@
+import Foundation
+import XCTest
+
+@testable import xcodeproj
+
+final class XCBuildConfigurationSpec: XCTestCase {
+    
+    var subject: XCBuildConfiguration!
+    
+    override func setUp() {
+        super.setUp()
+        subject = XCBuildConfiguration(reference: "reference",
+                                       baseConfigurationReference: nil,
+                                       buildSettings: ["name": "value"],
+                                       name: "Debug")
+    }
+    
+    func test_addingBuildSetting_returnsABuildConfigurationWithTheSettingAdded() {
+        let got = subject.addingBuild(setting: "name2", value: "value2")
+        XCTAssertEqual(got.buildSettings["name2"], "value2")
+    }
+    
+    func test_removingBuildSetting_returnsABuildConfigurationWithTheSettingRemoved() {
+        let got = subject.removingBuild(setting: "name")
+        XCTAssertNil(got.buildSettings["name"])
+    }
+}
