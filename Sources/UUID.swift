182aa029ba9301937a58a34d982acd4aa3285b29
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-04-24 18:16:21 +0200

    Add generateUUID function

--- Sources/UUID.swift
+++ Sources/UUID.swift
@@ -1,4 +1,9 @@
 import Foundation
+import CryptoSwift
 
 // Object reference
 public typealias UUID = String
+
+internal func generateUUID(path: String) -> String {
+    return path.md5().uppercased()
+}
