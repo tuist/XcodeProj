a460d2609197c62a848ff7ff66d6171841a96209
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-05-04 19:38:07 +0200

    Add new init to PBXVariantGroup that initialises the entity from a reference  and a dictionary

--- Sources/PBXVariantGroup.swift
+++ Sources/PBXVariantGroup.swift
@@ -1,12 +1,15 @@
 import Foundation
+import Unbox
 
 // This is the element for referencing localized resources.
 public struct PBXVariantGroup {
     
     // MARK: - Attributes
     
+    // Variant group reference.
     public let reference: UUID
     
+    // Variant group isa.
     public let isa: String = "PBXVariantGroup"
     
     // The objects are a reference to a PBXFileElement element
@@ -15,10 +18,18 @@ public struct PBXVariantGroup {
     // The filename
     public let name: String
     
+    // Variant group source tree.
     public let sourceTree: PBXSourceTree
     
     // MARK: - Init
     
+    /// Initializes the PBXVariantGroup with its values.
+    ///
+    /// - Parameters:
+    ///   - reference: variant group reference.
+    ///   - children: group children references.
+    ///   - name: name of the variant group
+    ///   - sourceTree: the group source tree.
     public init(reference: UUID,
                 children: [UUID],
                 name: String,
@@ -27,5 +38,47 @@ public struct PBXVariantGroup {
         self.children = children
         self.name = name
         self.sourceTree = sourceTree
+    }
+    
+    /// Initializes the variant group with the element reference an a dictionary with its properties.
+    ///
+    /// - Parameters:
+    ///   - reference: element reference.
+    ///   - dictionary: dictionary with the element properties.
+    /// - Throws: an error in case any property is missing or the format is wrong.
+    public init(reference: UUID, dictionary: [String: Any]) throws {
+        self.reference = reference
+        let unboxer = Unboxer(dictionary: dictionary)
+        self.children = try unboxer.unbox(key: "children")
+        self.name = try unboxer.unbox(key: "name")
+        self.sourceTree = try unboxer.unbox(key: "sourceTree")
+    }
+    
+    /// Returns a new variant group by adding a new children.
+    ///
+    /// - Parameter child: child to be added.
+    /// - Returns: new variant group with the child added.
+    public func adding(child: UUID) -> PBXVariantGroup {
+        var children = self.children
+        children.append(child)
+        return PBXVariantGroup(reference: reference,
+                               children: children,
+                               name: name,
+                               sourceTree: sourceTree)
+    }
+    
+    /// Returns a new new variant group removing one of its children.
+    ///
+    /// - Parameter child: child to be removed.
+    /// - Returns: new variant group with the child removed.
+    public func removing(child: UUID) -> PBXVariantGroup {
+        var children = self.children
+        if let index = children.index(of: child) {
+            children.remove(at: index)
+        }
+        return PBXVariantGroup(reference: reference,
+                               children: children,
+                               name: name,
+                               sourceTree: sourceTree)
     }
 }
--- Tests/xcodeprojTests/PBXVariantGroupSpec.swift
+++ Tests/xcodeprojTests/PBXVariantGroupSpec.swift
@@ -15,8 +15,69 @@ final class PBXVariantGroupSpec: XCTestCase {
                                        sourceTree: .group)
     }
     
+    func test_init_initializesTheModelWithTheCorrectAttributes() {
+        XCTAssertEqual(subject.reference, "reference")
+        XCTAssertEqual(subject.children, ["child"])
+        XCTAssertEqual(subject.name, "name")
+        XCTAssertEqual(subject.sourceTree, .group)
+    }
+    
+    func test_init_failsIfTheChildrenIsMissing() {
+        var dictionary = testDictionary()
+        dictionary.removeValue(forKey: "children")
+        do {
+            _ = try PBXVariantGroup(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didn't")
+        } catch {}
+    }
+    
+    func test_init_failsIfTheNameIsMissing() {
+        var dictionary = testDictionary()
+        dictionary.removeValue(forKey: "name")
+        do {
+            _ = try PBXVariantGroup(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didn't")
+        } catch {}
+    }
+    
+    func test_init_failsIfTheSourceTreeIsMissing() {
+        var dictionary = testDictionary()
+        dictionary.removeValue(forKey: "sourceTree")
+        do {
+            _ = try PBXVariantGroup(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didn't")
+        } catch {}
+    }
+    
+    func test_init_failsIfTheSourceTreeIsWrong() {
+        var dictionary = testDictionary()
+        dictionary["sourceTree"] = "asdgasdgas"
+        do {
+            _ = try PBXVariantGroup(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didn't")
+        } catch {}
+    }
+    
     func test_itHasTheCorrectIsa() {
         XCTAssertEqual(subject.isa, "PBXVariantGroup")
+    }
+    
+    func test_addingChild_returnsAVariantGroupWithTheChildAdded() {
+        let newVariant = subject.adding(child: "444")
+        XCTAssertTrue(newVariant.children.contains("444"))
+    }
+    
+    func test_removingChild_returnsAVariantGroupWithTheChildRemoved() {
+        let newVariant = subject.removing(child: "child")
+        XCTAssertFalse(newVariant.children.contains("child"))
+    }
+    
+    private func testDictionary() -> [String: Any] {
+        return [
+            "children": ["child1", "child2"],
+            "name": "name",
+            "sourceTree": "SDKROOT"
+        ]
     }
     
 }
