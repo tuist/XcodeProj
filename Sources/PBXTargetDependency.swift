00a8be31e487b3e6b8175a324aea76d1567dea2d
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-05-04 19:45:39 +0200

    Add new init to the PBXTargetDependency model to initialize it with the reference and a dictionary.

--- Sources/PBXTargetDependency.swift
+++ Sources/PBXTargetDependency.swift
@@ -1,16 +1,48 @@
 import Foundation
+import Unbox
 
 // This is the element for referencing other target through content proxies.
-public struct PBXTargetDependency {
+public struct PBXTargetDependency: Isa {
     
     // MARK: - Attributes
     
+    /// Target dependency reference.
     public let reference: UUID
     
+    /// Target dependency isa.
     public let isa: String = "PBXTargetDependency"
     
+    /// Target reference.
     public let target: UUID
     
+    /// Target proxy
     public let targetProxy: UUID
+    
+    /// Initializes the target dependency.
+    ///
+    /// - Parameters:
+    ///   - reference: element reference.
+    ///   - target: element target.
+    ///   - targetProxy: element target proxy.
+    public init(reference: UUID,
+                target: UUID,
+                targetProxy: UUID) {
+        self.reference = reference
+        self.target = target
+        self.targetProxy = targetProxy
+    }
+    
+    /// Initializes the target dependency with its reference and a dictionary that contains its attributes.
+    ///
+    /// - Parameters:
+    ///   - reference: element reference.
+    ///   - dictionary: dictionary with the attributes.
+    /// - Throws: throws an error in case of any attribute is missing or the type is not the expected one.
+    public init(reference: String, dictionary: [String: Any]) throws {
+        self.reference = reference
+        let unboxer = Unboxer(dictionary: dictionary)
+        self.target = try unboxer.unbox(key: "target")
+        self.targetProxy = try unboxer.unbox(key: "targetProxy")
+    }
     
 }
--- Tests/xcodeprojTests/PBXTargetDependencySpec.swift
+++ Tests/xcodeprojTests/PBXTargetDependencySpec.swift
@@ -13,7 +13,38 @@ final class PBXTargetDependencySpec: XCTestCase {
                                       targetProxy: "target_proxy")
     }
     
+    func test_init_initializesTheTargetDependencyWithTheCorrectAttributes() {
+        XCTAssertEqual(subject.reference, "reference")
+        XCTAssertEqual(subject.target, "target")
+        XCTAssertEqual(subject.targetProxy, "target_proxy")
+    }
+    
+    func test_init_failsIfTheTargetIsMissing() {
+        var dictionary = testDictionary()
+        dictionary.removeValue(forKey: "target")
+        do {
+            _ = try PBXTargetDependency(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didn't")
+        } catch {}
+    }
+    
+    func test_init_failsIfTheTargetProxyIsMissing() {
+        var dictionary = testDictionary()
+        dictionary.removeValue(forKey: "targetProxy")
+        do {
+            _ = try PBXTargetDependency(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didn't")
+        } catch {}
+    }
+    
     func test_hasTheCorrectIsa() {
         XCTAssertEqual(subject.isa, "PBXTargetDependency")
+    }
+    
+    private func testDictionary() -> [String: Any] {
+        return [
+            "target": "target",
+            "targetProxy": "targetProxy"
+        ]
     }
 }
