974bc9c2660b4b98d750a771a20f527dc2e51e50
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-05-05 16:56:52 +0200

    Add new initialisers to the PBXShellScriptBuildPhase and build methods and test them

--- Sources/PBXShellScriptBuildPhase.swift
+++ Sources/PBXShellScriptBuildPhase.swift
@@ -1,18 +1,192 @@
 import Foundation
+import Unbox
 
 // This is the element for the resources copy build phase.
-public struct PBXShellScriptBuildPhase {
+public struct PBXShellScriptBuildPhase: Isa, Hashable {
 
     // MARK: - Attributes
     
+    /// Element reference.
     public let reference: UUID
+    
+    /// Element isa.
     public let isa: String = "PBXShellScriptBuildPhase"
+    
+    /// Build action mask
     public let buildActionMask: Int = 2147483647
+
+    /// Files references
     public let files: [UUID]
+
+    /// Input paths
     public let inputPaths: [String]
+
+    /// Output paths
     public let outputPaths: [String]
+
+    /// Run only for deployment post processing attribute.
     public let runOnlyForDeploymentPostprocessing: Int = 0
+
+    /// Path to the shell.
     public let shellPath: String
-    public let shellScript: String
+
+    /// Shell script.
+    public let shellScript: String?
+
+    // MARK: - Init
+    
+    /// Initializes the shell script build phase with its attributes.
+    ///
+    /// - Parameters:
+    ///   - reference: references.
+    ///   - files: shell script files.
+    ///   - inputPaths: input paths.
+    ///   - outputPaths: output paths.
+    ///   - shellPath: shell path.
+    ///   - shellScript: shell script.
+    init(reference: UUID,
+         files: [UUID],
+         inputPaths: [String],
+         outputPaths: [String],
+         shellPath: String,
+         shellScript: String?) {
+        self.reference = reference
+        self.files = files
+        self.inputPaths = inputPaths
+        self.outputPaths = outputPaths
+        self.shellPath = shellPath
+        self.shellScript = shellScript
+    }
+    
+    /// Initializes the shell script build phase with its reference and a dictionary that contains its attributes.
+    ///
+    /// - Parameters:
+    ///   - reference: build phase reference.
+    ///   - dictionary: dictionary with the build phase attributes.
+    /// - Throws: throws an error in case any of the parameters is missing or it has the wrong type.
+    public init(reference: UUID, dictionary: [String: Any]) throws {
+        self.reference = reference
+        let unboxer = Unboxer(dictionary: dictionary)
+        self.files = try unboxer.unbox(key: "files")
+        self.inputPaths = (unboxer.unbox(key: "inputPaths")) ?? []
+        self.outputPaths = (unboxer.unbox(key: "outputPaths")) ?? []
+        self.shellPath = try unboxer.unbox(key: "shellPath")
+        self.shellScript = unboxer.unbox(key: "shellScript")
+    }
+    
+    // MARK: - Public
+    
+    /// Returns a new shell script build phase with a new file added.
+    ///
+    /// - Parameter file: reference file to be added.
+    /// - Returns: new build phase with the file added.
+    public func adding(file: UUID) -> PBXShellScriptBuildPhase {
+        var files = self.files
+        files.append(file)
+        return PBXShellScriptBuildPhase(reference: reference,
+                                        files: files,
+                                        inputPaths: inputPaths,
+                                        outputPaths: outputPaths,
+                                        shellPath: shellPath,
+                                        shellScript: shellScript)
+    }
+    
+    /// Returns a new shell script build phase removing a file.
+    ///
+    /// - Parameter file: reference to the file to be removed.
+    /// - Returns: new shell script build phase with the file removed.
+    public func removing(file: UUID) -> PBXShellScriptBuildPhase {
+        var files = self.files
+        if let index = files.index(of: file) {
+            files.remove(at: index)
+        }
+        return PBXShellScriptBuildPhase(reference: reference,
+                                        files: files,
+                                        inputPaths: inputPaths,
+                                        outputPaths: outputPaths,
+                                        shellPath: shellPath,
+                                        shellScript: shellScript)
+    }
+    
+    /// Returns a new shell script build phase adding a new input path.
+    ///
+    /// - Parameter inputPath: input path to be added.
+    /// - Returns: new shell script build phase with the input path added.
+    public func adding(inputPath: String) -> PBXShellScriptBuildPhase {
+        var inputPaths = self.inputPaths
+        inputPaths.append(inputPath)
+        return PBXShellScriptBuildPhase(reference: reference,
+                                        files: files,
+                                        inputPaths: inputPaths,
+                                        outputPaths: outputPaths,
+                                        shellPath: shellPath,
+                                        shellScript: shellScript)
+    }
+    
+    /// Returns a new shell script build phase removing an input path/
+    ///
+    /// - Parameter inputPath: input path to be removed.
+    /// - Returns: new shell script build phase with the input path removed.
+    public func removing(inputPath: String) -> PBXShellScriptBuildPhase {
+        var inputPaths = self.inputPaths
+        if let index = inputPaths.index(of: inputPath) {
+            inputPaths.remove(at: index)
+        }
+        return PBXShellScriptBuildPhase(reference: reference,
+                                        files: files,
+                                        inputPaths: inputPaths,
+                                        outputPaths: outputPaths,
+                                        shellPath: shellPath,
+                                        shellScript: shellScript)
+    }
+    
+    /// Returns a new shell script build phase adding an output path.
+    ///
+    /// - Parameter outputPath: output path to be added.
+    /// - Returns: new shell script build phsae with the output path added.
+    public func adding(outputPath: String) -> PBXShellScriptBuildPhase {
+        var outputPaths = self.outputPaths
+        outputPaths.append(outputPath)
+        return PBXShellScriptBuildPhase(reference: reference,
+                                        files: files,
+                                        inputPaths: inputPaths,
+                                        outputPaths: outputPaths,
+                                        shellPath: shellPath,
+                                        shellScript: shellScript)
+    }
+    
+    /// Returns a new shell script build phase removing an output path.
+    ///
+    /// - Parameter outputPath: output path to be removed.
+    /// - Returns: new shell script build phsae with the output path removed.
+    public func removing(outputPath: String) -> PBXShellScriptBuildPhase {
+        var outputPaths = self.outputPaths
+        if let index = outputPaths.index(of: outputPath) {
+            outputPaths.remove(at: index)
+        }
+        return PBXShellScriptBuildPhase(reference: reference,
+                                        files: files,
+                                        inputPaths: inputPaths,
+                                        outputPaths: outputPaths,
+                                        shellPath: shellPath,
+                                        shellScript: shellScript)
+    }
+
+    // MARK: - Hashable
+    
+    public static func == (lhs: PBXShellScriptBuildPhase,
+                           rhs: PBXShellScriptBuildPhase) -> Bool {
+        return lhs.reference == rhs.reference &&
+        lhs.isa == rhs.isa &&
+        lhs.buildActionMask == rhs.buildActionMask &&
+        lhs.files == rhs.files &&
+        lhs.inputPaths == rhs.inputPaths &&
+        lhs.outputPaths == rhs.outputPaths &&
+        lhs.runOnlyForDeploymentPostprocessing == rhs.runOnlyForDeploymentPostprocessing &&
+        lhs.shellPath == rhs.shellPath &&
+        lhs.shellScript == rhs.shellScript
+    }
+    
+    public var hashValue: Int { return self.reference.hashValue }
 
 }
---
+++ Tests/xcodeprojTests/PBXShellScriptBuildPhaseSpec.swift
@@ -0,0 +1,95 @@
+import Foundation
+import XCTest
+
+@testable import xcodeproj
+
+final class PBXShellScriptBuildPhaseSpec: XCTestCase {
+    
+    var subject: PBXShellScriptBuildPhase!
+    
+    override func setUp() {
+        super.setUp()
+        self.subject = PBXShellScriptBuildPhase(reference: "reference", files: ["file"], inputPaths: ["input"], outputPaths: ["output"], shellPath: "shell", shellScript: "script")
+    }
+
+    func test_init_initializesTheBuildPhaseWithTheCorrectValues() {
+        XCTAssertEqual(subject.reference, "reference")
+        XCTAssertEqual(subject.files, ["file"])
+        XCTAssertEqual(subject.inputPaths, ["input"])
+        XCTAssertEqual(subject.outputPaths, ["output"])
+        XCTAssertEqual(subject.shellPath, "shell")
+        XCTAssertEqual(subject.shellScript, "script")
+    }
+    
+    func test_returnsTheCorrectIsa() {
+        XCTAssertEqual(subject.isa, "PBXShellScriptBuildPhase")
+    }
+    
+    func test_initFails_whenTheFilesAreMissing() {
+        var dictionary = testDictionary()
+        dictionary.removeValue(forKey: "files")
+        do {
+            _ = try PBXShellScriptBuildPhase(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didnt'")
+        } catch {}
+    }
+    
+    func test_initFails_whenShellPathIsMissing() {
+        var dictionary = testDictionary()
+        dictionary.removeValue(forKey: "shellPath")
+        do {
+            _ = try PBXShellScriptBuildPhase(reference: "reference", dictionary: dictionary)
+            XCTAssertTrue(false, "Expected to throw an error but it didnt'")
+        } catch {}
+    }
+    
+    func test_addingFile_returnsABuildPhaseWithTheFileAdded() {
+        let got = subject.adding(file: "file2")
+        XCTAssertTrue(got.files.contains("file2"))
+    }
+    
+    func test_removingFile_returnsABuildPhaseWithTheFileRemoved() {
+        let got = subject.removing(file: "file")
+        XCTAssertFalse(got.files.contains("file"))
+    }
+    
+    func test_addingInputFile_returnsABuildPhaseAddingTheInputFile() {
+        let got = subject.adding(inputPath: "input2")
+        XCTAssertTrue(got.inputPaths.contains("input2"))
+    }
+    
+    func test_removingInputFile_returnsABuildPhaseRemovingTheInputFile() {
+        let got = subject.removing(inputPath: "input")
+        XCTAssertFalse(got.inputPaths.contains("input"))
+    }
+    
+    func test_addingOutputFile_returnsABuildPhaseAddingTheOutputFile() {
+        let got = subject.adding(outputPath: "output2")
+        XCTAssertTrue(got.outputPaths.contains("output2"))
+    }
+    
+    func test_removingOutputFile_returnsABuildPhaseRemovingTheOutputFile() {
+        let got = subject.removing(outputPath: "output")
+        XCTAssertFalse(got.outputPaths.contains("output"))
+    }
+    
+    func test_equals_returnsTheCorrectValue() {
+        let one = PBXShellScriptBuildPhase(reference: "reference", files: ["file"], inputPaths: ["input"], outputPaths: ["output"], shellPath: "shell", shellScript: "script")
+        let another = PBXShellScriptBuildPhase(reference: "reference", files: ["file"], inputPaths: ["input"], outputPaths: ["output"], shellPath: "shell", shellScript: "script")
+        XCTAssertEqual(one, another)
+    }
+    
+    func test_hashValue_returnsTheReferenceHashValue() {
+        XCTAssertEqual(subject.hashValue, subject.reference.hashValue)
+    }
+    
+    private func testDictionary() -> [String: Any] {
+        return [
+            "files": ["files"],
+            "inputPaths": ["input"],
+            "outputPaths": ["output"],
+            "shellPath": "shellPath",
+            "shellScript": "shellScript"
+        ]
+    }
+}
