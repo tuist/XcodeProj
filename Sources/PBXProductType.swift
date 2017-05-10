24a0a35c858bd23b1568ace40cf1c5de08595058
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-05-10 17:57:22 +0200

    Define PBXProductType

---
+++ Sources/PBXProductType.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Unbox
+
+/// Product type.
+///
+/// - none: None
+/// - application: Application
+/// - tool: Tool
+/// - libraryStatic: Static Library
+/// - libraryDynamic: Dynamic Library.
+/// - kernelExtension: Kernel Extension.
+/// - kernelExtensionIOKit: Kernel Extension IO Kit
+public enum PBXProductType: String, UnboxableEnum {
+    case none = ""
+    case application = "com.apple.product-type.application"
+    case tool = "com.apple.product-type.tool"
+    case libraryStatic = "com.apple.product-type.library.static"
+    case libraryDynamic = "com.apple.product-type.library.dynamic"
+    case kernelExtension = "com.apple.product-type.kernel-extension"
+    case kernelExtensionIOKit = "com.apple.product-type.kernel-extension.iokit"
+}
