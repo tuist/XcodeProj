34ce9782c8027a7cc00af8738ff5adc00a4ec287
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-05-12 10:49:08 +0200

    Update validators swift file adding the validations that must be implemented.

--- Sources/Validators.swift
+++ Sources/Validators.swift
@@ -1,7 +1,16 @@
 /*
  - XCConfigurationList has existing configurations
  - XCConfigurationList defaultConfigurationName exists
- 
- 
- 
+ - PBXTargetDependency is proxying to a target that exists 
+ - PBXTargetDependency is proxying to a PBXContainerItemProxy that exists.
+ - PBXContainerItemProxy is pointing to a containerPortal that exists.
+ - PBXContainerItemProxy is pointing to a remoteGlobalIDString that exists.
+ - PBXSourcesBuildPhase files is pointing to a file that doesn't exist.
+ - PBXShellScriptBuildPhase files is pointing to a file that doesn't exist.
+ - PBXGroup files pointing to files that don't exist.
+ - PBXNativeTarget is referencing to an existing configuration list.
+ - PBXNativeTarget is refering to existing build phases.
+ - PBXNativeTarget is refering to existing dependencies
+ - PBXNativeTarget is refering to a valid product reference.
+ - PBXFrameworksBuildPhase is refering to existing files.
 */
