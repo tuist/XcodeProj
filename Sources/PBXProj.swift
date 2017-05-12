a237d0e7016522abeb86740bc77a546bef7fcf2a
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-05-12 10:48:23 +0200

    Add missing documentation in PBXProj

--- Sources/PBXProj.swift
+++ Sources/PBXProj.swift
@@ -7,33 +7,97 @@ public struct PBXProj {
     
     // MARK: - Properties
     
+    /// Project path.
     public let path: Path
+    
+    /// Project archive version.
     public let archiveVersion: Int
+    
+    /// Project object version.
     public let objectVersion: Int
-    public let classes: [Any]
+    
+    /// Project classes.
+    public let classes: Set<PBXObject>
+    
+    /// Project root object.
     public let rootObject: UUID
     
+    // MARK: - Init
+    
+    /// Initializes the project with its attributes.
+    ///
+    /// - Parameters:
+    ///   - path: project path.
+    ///   - archiveVersion: project archive version.
+    ///   - objectVersion: project object version.
+    ///   - classes: project classes.
+    ///   - rootObject: project root object.
+    public init(path: Path,
+                archiveVersion: Int,
+                objectVersion: Int,
+                classes: Set<PBXObject>,
+                rootObject: UUID) {
+        self.path = path
+        self.archiveVersion = archiveVersion
+        self.objectVersion = objectVersion
+        self.classes = classes
+        self.rootObject = rootObject
+    }
 }
 
 // MARK: - PBXProj Extension (Dictionary Initialization)
 
-internal extension PBXProj {
+extension PBXProj: PlistInitiatable {
     
-    init(path: Path, dictionary: [String: Any]) throws {
+    /// Initializes the .pbxproj representation with the path where the file is and a dictionary with its content.
+    ///
+    /// - Parameters:
+    ///   - path: path where the .pbxproj file is.
+    ///   - dictionary: dictionary with the file content.
+    /// - Throws: throws an error if the content cannot be parsed properly.
+    public init(path: Path, dictionary: [String: Any]) throws {
         self.path = path
         let unboxer = Unboxer(dictionary: dictionary)
         self.archiveVersion = try unboxer.unbox(key: "archiveVersion")
         self.objectVersion = try unboxer.unbox(key: "objectVersion")
-        self.classes = (dictionary["classes"] as? [Any]) ?? []
+        //sss
+        self.classes = []
+//        self.classes = (dictionary["classes"] as? [Any]) ?? []
         self.rootObject = try unboxer.unbox(key: "rootObject")
     }
     
 }
 
-//public extension PBXProj {
-//    
-//    init(path: ) {
-//
-//    }
-//    
-//}
+// MARK: - PBXProj Extension (Classes Accessors)
+
+public extension PBXProj {
+    
+    /// Returns a new project by adding a class.
+    ///
+    /// - Parameter projectClass: class to be added.
+    /// - Returns: project with the new class added.
+    public func adding(projectClass: PBXObject) -> PBXProj {
+        var classes = self.classes
+        classes.insert(projectClass)
+        return PBXProj(path: path,
+                       archiveVersion: archiveVersion,
+                       objectVersion: objectVersion,
+                       classes: classes,
+                       rootObject: rootObject)
+    }
+    
+    /// Returns a new project removing a class.
+    ///
+    /// - Parameter projectClass: class to be removed
+    /// - Returns: project with the classs added.
+    public func removing(projectClass: PBXObject) -> PBXProj {
+        var classes = self.classes
+        classes.remove(projectClass)
+        return PBXProj(path: path,
+                       archiveVersion: archiveVersion,
+                       objectVersion: objectVersion,
+                       classes: classes,
+                       rootObject: rootObject)
+    }
+    
+}
