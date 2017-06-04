f7a9ac3d147cbdbfe6bd9a22eef651a7a6184732
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-06-04 00:44:05 -0700

    Fix Swift Package.swift file format

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
             name: "Models",
             dependencies: ["Extensions", "Protocols"])
     ],
+    swiftLanguageVersions: [3],
     dependencies: [
         .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/JohnSundell/Unbox", majorVersion: 2, minor: 4),
         .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/drmohundro/SWXMLHash", majorVersion: 4, minor: 1)
-    ],
-    swiftLanguageVersions: [3]
+    ]
 )
