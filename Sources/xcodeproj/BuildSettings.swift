a9045ca233458e1ff1643b96ce24ee6c9557ad39
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-06-13 17:14:55 -0700

    Define BuildSettings model

---
+++ Sources/xcodeproj/BuildSettings.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+/// Struct that represents the Xcode build settings.
+public struct BuildSettings {
+    
+    // MARK: - Attributes
+    
+    var dictionary: [String: String]
+    
+    // MARK: - Init
+    
+    /// Initializes the build settings with the dictionary that contains all the settings.
+    ///
+    /// - Parameter dictionary: build settings dictionary.
+    public init(dictionary: [String: String]) {
+        self.dictionary = dictionary
+    }
+    
+    // MARK: - Public
+    
+    /// Returns the value for a given setting.
+    ///
+    /// - Parameter key: setting whose value will be returned.
+    public subscript(key: String) -> String? {
+        get {
+            return self.dictionary[key]
+        }
+        set {
+            self.dictionary[key] = newValue
+        }
+    }
+    
+}
+
+// MARK: - BuildSettings Extension (ExpressibleByDictionaryLiteral)
+
+extension BuildSettings: ExpressibleByDictionaryLiteral {
+    
+    public init(dictionaryLiteral elements: (String, String)...) {
+        var dictionary: [String: String] = [:]
+        elements.forEach { dictionary[$0.0] = $0.1 }
+        self.dictionary = dictionary
+    }
+    
+}
