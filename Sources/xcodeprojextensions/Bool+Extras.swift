3e70664ddc3a6a3d80a2931f805afa4db3db34be
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-06-13 17:16:22 -0700

    Add comments to the Bool extension

--- Sources/xcodeprojextensions/Bool+Extras.swift
+++ Sources/xcodeprojextensions/Bool+Extras.swift
@@ -1,7 +1,13 @@
 import Foundation
 
+
+// MARK: - Bool Extension (Extras)
+
 public extension Bool {
+
+    /// Returns a XML string value that represents the boolean.
     var xmlString: String {
         return self ? "YES": "NO"
     }
+
 }
