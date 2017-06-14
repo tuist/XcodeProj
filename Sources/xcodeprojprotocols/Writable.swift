c939cbd62e3b32948d622c3a58c36f90e64938b7
Pedro Piñera Buendía <pepibumur@gmail.com>
2017-06-13 17:14:38 -0700

    Document Writable protocol

--- Sources/xcodeprojprotocols/Writable.swift
+++ Sources/xcodeprojprotocols/Writable.swift
@@ -2,5 +2,11 @@ import Foundation
 
 /// Protocol that defines how an entity can be writed into disk
 public protocol Writable {
+    
+    /// Writes the object that conforms the protocol.
+    ///
+    /// - Parameter override: True if the content should be overriden if it already exists.
+    /// - Throws: writing error if something goes wrong.
     func write(override: Bool) throws
+
 }
