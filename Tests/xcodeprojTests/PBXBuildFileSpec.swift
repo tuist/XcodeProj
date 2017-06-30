ad03a68b497b3b18d70f95352493940dfd90511f
Pedro Piñera <pepibumur@gmail.com>
2017-06-30 16:52:49 +0200

    Group imports

--- Tests/xcodeprojTests/PBXBuildFileSpec.swift
+++ Tests/xcodeprojTests/PBXBuildFileSpec.swift
@@ -1,6 +1,5 @@
 import Foundation
 import XCTest
-
 import xcodeproj
 
 final class PBXBuildFileSpec: XCTestCase {
--- Tests/xcodeprojTests/PBXCopyFilesBuildPhaseSpec.swift
+++ Tests/xcodeprojTests/PBXCopyFilesBuildPhaseSpec.swift
@@ -1,6 +1,5 @@
 import Foundation
 import XCTest
-
 import xcodeproj
 
 final class PBXCopyFilesBuildPhaseSpec: XCTestCase {
