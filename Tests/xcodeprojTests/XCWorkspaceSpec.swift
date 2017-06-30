0da8c7e6a281cff6eb0fa86b2ac2a581de67bbe5
Pedro Piñera <pepibumur@gmail.com>
2017-06-30 16:34:05 +0200

    [] Rename XCWorspaceSpec to XCWorkspaceIntegrationSpec

--- Tests/xcodeprojTests/XCWorkspaceSpec.swift
+++ Tests/xcodeprojTests/XCWorkspaceSpec.swift
@@ -1,19 +1,18 @@
 import Foundation
 import XCTest
 import PathKit
-
 import xcodeproj
 
-final class XCWorkspaceSpec: XCTestCase {
+final class XCWorkspaceIntegrationSpec: XCTestCase {
     
-    func test_integration_initTheWorkspaceWithTheRightPropeties() {
+    func test_initTheWorkspaceWithTheRightPropeties() {
         let fixtures = Path(#file).parent().parent().parent() + Path("Fixtures")
         let path = fixtures + Path("iOS/Project.xcodeproj/project.xcworkspace")
         let got = try? XCWorkspace(path: path)
         XCTAssertNotNil(got)
     }
     
-    func test_integration_initFailsIfThePathIsWrong() {
+    func test_initFailsIfThePathIsWrong() {
         do {
             _ = try XCWorkspace(path: Path("test"))
             XCTAssertTrue(false, "Expected to throw an error but it didn't")
