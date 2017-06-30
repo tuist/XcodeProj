aea18132e3c96643f0c67cdbc320f07bed4d82eb
Pedro Piñera <pepibumur@gmail.com>
2017-06-30 16:39:24 +0200

    Minor fixes

--- Tests/xcodeprojTests/XCBuildConfigurationSpec.swift
+++ Tests/xcodeprojTests/XCBuildConfigurationSpec.swift
@@ -34,7 +34,7 @@ final class XCBuildConfigurationSpec: XCTestCase {
     
     func test_addingBuildSetting_returnsABuildConfigurationWithTheSettingAdded() {
         let got = subject.addingBuild(setting: "name2", value: "value2")
-        XCTAssertEqual(got.buildSettings["name2"] as! String, "value2")
+        XCTAssertEqual(got.buildSettings["name2"]!, "value2")
     }
     
     func test_removingBuildSetting_returnsABuildConfigurationWithTheSettingRemoved() {
