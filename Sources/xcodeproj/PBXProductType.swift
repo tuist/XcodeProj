f8d438e466daab207f027c3dd470bf32f602b091
Pedro Piñera <pepibumur@gmail.com>
2017-07-16 13:01:20 +0200

    Add missing product types

--- Sources/xcodeproj/PBXProductType.swift
+++ Sources/xcodeproj/PBXProductType.swift
@@ -1,23 +1,24 @@
 import Foundation
 import Unbox
 
-/// Product type.
-///
-/// - none: None
-/// - application: Application
-/// - tool: Tool
-/// - libraryStatic: Static Library
-/// - libraryDynamic: Dynamic Library.
-/// - kernelExtension: Kernel Extension.
-/// - kernelExtensionIOKit: Kernel Extension IO Kit
-/// - bundleUnitTest: Bundle unit test
 public enum PBXProductType: String, UnboxableEnum {
     case none = ""
     case application = "com.apple.product-type.application"
-    case tool = "com.apple.product-type.tool"
-    case libraryStatic = "com.apple.product-type.library.static"
-    case libraryDynamic = "com.apple.product-type.library.dynamic"
-    case kernelExtension = "com.apple.product-type.kernel-extension"
-    case kernelExtensionIOKit = "com.apple.product-type.kernel-extension.iokit"
-    case bundleUnitTest = "com.apple.product-type.bundle.unit-test"
+    case framework = "com.apple.product-type.framework"
+    case dynamicLibrary = "com.apple.product-type.library.dynamic"
+    case staticLibrary = "com.apple.product-type.library.static"
+    case bundle = "com.apple.product-type.bundle"
+    case unitTestBundle = "com.apple.product-type.bundle.unit-test"
+    case uiTestBundle = "com.apple.product-type.bundle.ui-testing"
+    case appExtension = "com.apple.product-type.app-extension"
+    case commandLineTool = "com.apple.product-type.tool"
+    case watchApp = "com.apple.product-type.application.watchapp"
+    case watch2App = "com.apple.product-type.application.watchapp2"
+    case watchExtension = "com.apple.product-type.watchkit-extension"
+    case watch2Extension = "com.apple.product-type.watchkit2-extension"
+    case tvExtension = "com.apple.product-type.tv-app-extension"
+    case messagesApplication = "com.apple.product-type.application.messages"
+    case messagesExtension = "com.apple.product-type.app-extension.messages"
+    case stickerPack = "com.apple.product-type.app-extension.messages-sticker-pack"
+    case xpcService = "com.apple.product-type.xpc-service"
 }
--- Tests/xcodeprojTests/PBXProductTypeSpec.swift
+++ Tests/xcodeprojTests/PBXProductTypeSpec.swift
@@ -13,23 +13,71 @@ final class PBXProductTypeSpec: XCTestCase {
         XCTAssertEqual(PBXProductType.application.rawValue, "com.apple.product-type.application")
     }
     
-    func test_tool_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.tool.rawValue, "com.apple.product-type.tool")
+    func test_framework_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.framework.rawValue, "com.apple.product-type.framework")
     }
     
-    func test_libraryStatic_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.libraryStatic.rawValue, "com.apple.product-type.library.static")
+    func test_dynamicLibrary_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.dynamicLibrary.rawValue, "com.apple.product-type.library.dynamic")
     }
     
-    func test_libraryDynamic_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.libraryDynamic.rawValue, "com.apple.product-type.library.dynamic")
+    func test_staticLibrary_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.staticLibrary.rawValue, "com.apple.product-type.library.static")
     }
     
-    func test_kernelExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.kernelExtension.rawValue, "com.apple.product-type.kernel-extension")
+    func test_bundle_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.bundle.rawValue, "com.apple.product-type.bundle")
     }
     
-    func test_kernelExtensionIOKit_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.kernelExtensionIOKit.rawValue, "com.apple.product-type.kernel-extension.iokit")
+    func test_unitTestBundle_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.unitTestBundle.rawValue, "com.apple.product-type.bundle.unit-test")
+    }
+    
+    func test_uiTestBundle_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.uiTestBundle.rawValue, "com.apple.product-type.bundle.ui-testing")
+    }
+    
+    func test_appExtension_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.appExtension.rawValue, "com.apple.product-type.app-extension")
+    }
+    
+    func test_commandLineTool_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.commandLineTool.rawValue, "com.apple.product-type.tool")
+    }
+    
+    func test_watchApp_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.watchApp.rawValue, "com.apple.product-type.application.watchapp")
+    }
+    
+    func test_watch2App_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.watch2App.rawValue, "com.apple.product-type.application.watchapp2")
+    }
+    
+    func test_watchExtension_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.watchExtension.rawValue, "com.apple.product-type.watchkit-extension")
+    }
+    
+    func test_watch2Extension_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.watch2Extension.rawValue, "com.apple.product-type.watchkit2-extension")
+    }
+    
+    func test_tvExtension_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.tvExtension.rawValue, "com.apple.product-type.tv-app-extension")
+    }
+    
+    func test_messagesApplication_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.messagesApplication.rawValue, "com.apple.product-type.application.messages")
+    }
+    
+    func test_messagesExtension_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.messagesExtension.rawValue, "com.apple.product-type.app-extension.messages")
+    }
+    
+    func test_stickerPack_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.stickerPack.rawValue, "com.apple.product-type.app-extension.messages-sticker-pack")
+    }
+    
+    func test_xpcService_hasTheRightValue() {
+        XCTAssertEqual(PBXProductType.xpcService.rawValue, "com.apple.product-type.xpc-service")
     }
 }
