8be7107eaf96b26407b9d6016950400dff9c7780
Pedro Piñera <pepibumur@gmail.com>
2017-07-18 18:35:54 +0200

    [swift4] Remove Unbox dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ let package = Package(
     name: "xcodeproj",
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "0.8.0")),
-        .package(url: "https://github.com/JohnSundell/Unbox", .upToNextMinor(from: "2.4.0")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .branch("swift4")),
         .package(url: "https://github.com/tadija/AEXML.git", .upToNextMinor(from: "4.0.0"))
     ],
