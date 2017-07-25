8cbf3af360dccae1e30ca2371d697f2661a60039
Yonas Kolb <yonaskolb@gmail.com>
2017-07-25 20:42:14 +0200

    downgrade package to Swift 3

--- Package.swift
+++ Package.swift
@@ -1,42 +1,24 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.1
 
 import PackageDescription
 
 let package = Package(
     name: "xcodeproj",
-    dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", .exact("0.8.0")),
-        .package(url: "https://github.com/JohnSundell/Unbox", .exact("2.5.0")),
-        .package(url: "https://github.com/tadija/AEXML.git", .exact("4.1.0")),
-        .package(url: "https://github.com/Bouke/CCommonCrypto", .exact("1.0.0"))
-    ],
     targets: [
-        .target(
+        Target(
             name: "xcodeprojextensions",
-            dependencies: [],
-            path: "Sources/xcodeprojextensions"),
-        .target(
+            dependencies: []),
+        Target(
             name: "xcodeprojprotocols",
-            dependencies: ["xcodeprojextensions",
-                          "PathKit"],
-            path: "Sources/xcodeprojprotocols"),
-        .target(
+            dependencies: ["xcodeprojextensions"]),
+        Target(
             name: "xcodeproj",
-            dependencies: ["xcodeprojextensions",
-                          "xcodeprojprotocols",
-                          "Unbox",
-                          "PathKit",
-                          "AEXML"],
-            path: "Sources/xcodeproj"),
-        .testTarget(
-            name: "xcodeprojextensionsTests",
-            dependencies: ["xcodeprojextensions",
-                          "PathKit"],
-            path: "Tests/xcodeprojextensionsTests"),
-        .testTarget(
-            name: "xcodeprojTests",
-            dependencies: ["xcodeproj"],
-            path: "Tests/xcodeprojTests")
+            dependencies: ["xcodeprojextensions", "xcodeprojprotocols"])
     ],
-    swiftLanguageVersions: [3]
+    dependencies: [
+        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/JohnSundell/Unbox", majorVersion: 2, minor: 5),
+        .Package(url: "https://github.com/Bouke/CCommonCrypto.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 1)
+    ]
 )
