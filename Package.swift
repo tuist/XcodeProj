184a39c0f0ab1adb55733a15d8c162cfe3b0a29a
Artem Novichkov <artem.novichkov@rosberry.com>
2017-09-20 14:30:22 +0600

    Update SPM manifest with Swift 4

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "xcodeproj",
-    targets: [
-        Target(
-            name: "xcodeproj")
-    ],
     dependencies: [
-        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/JohnSundell/Unbox", majorVersion: 2, minor: 5),
-        .Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 1)
-    ]
+        .package(url: "https://github.com/kylef/PathKit.git", .upToNextMajor(from: "0.0.8")),
+        .package(url: "https://github.com/JohnSundell/Unbox.git", .upToNextMajor(from: "2.5.0")),
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.1.0")),
+        ],
+    targets: [
+        .target(
+            name: "xcodeproj",
+            dependencies: ["PathKit", "Unbox", "AEXML"]),
+        ]
 )
