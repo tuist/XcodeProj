1618e6c5dbd98d61990ad2a8e87b3e3a4ce30314
Pedro Piñera <pepibumur@gmail.com>
2017-10-01 01:20:18 +0200

    Test CocoaPods integration on CI

---
+++ CocoaPods/macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CocoaPods
+//
+//  Created by Pedro Piñera Buendía on 01.10.17.
+//  Copyright © 2017 xcode.swift. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
