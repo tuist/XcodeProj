07bcef4fe0175ae0341077c4e2226f4517001455
toshi0383 <t.suzuki326@gmail.com>
2017-10-01 17:16:58 +0900

    Fix wrong comment for XCConfigurationList reference

--- Tests/xcprojTests/XCConfigurationListSpec.swift
+++ Tests/xcprojTests/XCConfigurationListSpec.swift
@@ -1,6 +1,6 @@
 import Foundation
 import XCTest
-import xcproj
+@testable import xcproj
 
 final class XCConfigurationListSpec: XCTestCase {
 
@@ -17,4 +17,10 @@ final class XCConfigurationListSpec: XCTestCase {
         XCTAssertEqual(XCConfigurationList.isa, "XCConfigurationList")
     }
 
+    func test_plistKeyAndValue() {
+        let proj = PBXProj(archiveVersion: 1, objectVersion: 1, rootObject: "")
+        proj.projects = [PBXProject.init(name: "App", reference: "", buildConfigurationList: "reference", compatibilityVersion: "47", mainGroup: "")]
+        let (commentedString, _) = subject.plistKeyAndValue(proj: proj)
+        XCTAssertEqual(commentedString, CommentedString("reference", comment: "Build configuration list for PBXProject \"App\""))
+    }
 }
