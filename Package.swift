bff5e08409d7129362a4b3b359d61949b671a9ed
Pedro Piñera <pepibumur@gmail.com>
2017-12-11 22:13:59 +0100

    [multithread-parsing] Add BrightFutures dependency

--- Package.swift
+++ Package.swift
@@ -10,8 +10,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", .upToNextMajor(from: "0.8.0")),
         .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
-        ],
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
+        .package(url: "https://github.com/Thomvis/BrightFutures.git", from: "6.0.0")
+    ],
     targets: [
         .target(name: "xcproj", dependencies: ["PathKit", "AEXML"]),
         .testTarget(name: "xcprojTests", dependencies: ["xcproj"]),
