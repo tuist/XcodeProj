bccfc3a7267ec2155443fca81a4f39627efb3385
Pedro Piñera <pepibumur@gmail.com>
2017-12-13 12:53:39 +0100

    [danger-carthage] Add a Danger plugin to check if the Carthage project has been generated after files have been added or deleted from the Sources/xcproj folder

--- Danger/carthage.rb
+++ Danger/carthage.rb
@@ -6,8 +6,14 @@ module Danger
     class Carthage < Plugin
 
       def execute
+        if added_or_deleted_sources.empty?() then return end
+        fail("Source files have been added or removed. Execute `bundle exec rake generate_carthage_project` to regenerate the Carthage.xcodeproj")
+      end
 
+      private
 
+      def added_or_deleted_sources
+         (git.added_files + git.deleted_files).select {|path| path.include?("Sources/xcproj")}
       end
 
     end
--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ gem "colorize"
 gem "cocoapods"
 gem 'danger-xcodeswift', :git => "https://github.com/xcodeswift/danger", require: false
 gem "git"
+gem "rspec"
