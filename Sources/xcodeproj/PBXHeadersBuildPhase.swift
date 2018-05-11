1e3fdcd11bfe074f55260baed72b6108392fa401
Pedro <pepibumur@gmail.com>
2018-05-11 23:16:36 +0200

    Add BuildSettingsProvider

--- Sources/xcodeproj/PBXHeadersBuildPhase.swift
+++ Sources/xcodeproj/PBXHeadersBuildPhase.swift
@@ -8,27 +8,6 @@ public final class PBXHeadersBuildPhase: PBXBuildPhase {
     }
 }
 
-// MARK: - PBXHeadersBuildPhase Extension (Extras)
-
-extension PBXHeadersBuildPhase {
-    /// Returns if the path refers to a header file.
-    ///
-    /// - Parameter path: path to be checked.
-    /// - Returns: true if the path points to a header file.
-    static func isHeader(path: RelativePath) -> Bool {
-        return path.extension.flatMap({ isHeader(fileExtension: $0) }) ?? false
-    }
-
-    /// Returns if the given extension is a header.
-    ///
-    /// - Parameter fileExtension: file extension to be checked.
-    /// - Returns: true if the file represents a header.
-    static func isHeader(fileExtension: String) -> Bool {
-        let headersExtensions = ["h", "hh", "hpp", "ipp", "tpp", "hxx", "def"]
-        return headersExtensions.contains(fileExtension)
-    }
-}
-
 // MARK: - PBXHeadersBuildPhase Extension (PlistSerializable)
 
 extension PBXHeadersBuildPhase: PlistSerializable {
