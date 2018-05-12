7e75492edd05533e0e1e36651804e9932917ca58
Pedro <pepibumur@gmail.com>
2018-05-12 08:52:37 +0200

    Some improvements

--- Sources/xcodeproj/Xcode.swift
+++ Sources/xcodeproj/Xcode.swift
@@ -8,57 +8,57 @@ public struct Xcode {
         public struct SDK {
             /// Last known SDK for iOS.
             public static let ios: String = "11.3"
-
+            
             /// Last known SDK for macOS.
             public static let macos: String = "10.13"
-
+            
             /// Last known SDK for tvOS.
             public static let tvos: String = "10.2"
-
+            
             /// Last known SDK for watchos.
             public static let watchos: String = "3.2"
         }
-
+        
         /// Last known archive version for Xcodeproj.
         public static let archiveVersion: UInt = 1
-
+        
         /// Last known Swift version (stable).
         public static let swiftVersion = "4.1"
-
+        
         /// Last known object version for Xcodeproj.
         public static let objectVersion: UInt = 50
-
+        
         /// Last known upgrade check.
         public static let upgradeCheck = "0930"
-
+        
         /// Last known Swift upgrade check.
         public static let swiftUpgradeCheck = "0930"
     }
-
+    
     /// Default values.
     public struct Default {
         /// The default object version for Xcodeproj.
-        public static let objectVersion: UInt = 46
-
+        public static let objectVersion: UInt = 48
+        
         /// Default compatibility version.
         public static let compatibilityVersion: String = "Xcode 3.2"
-
+        
         /// Default development region.
-        public static let developmentRegion: String = "English"
+        public static let developmentRegion: String = "en"
     }
-
+    
     /// Inherited keywords used in build settings.
     public static let inheritedKeywords = ["${inherited}", "$(inherited)"]
-
+    
     /// Header files extensions.
     public static let headersExtensions = [".h", ".hh", ".hpp", ".ipp", ".tpp", ".hxx", ".def", ".inl"]
-
+    
     /// Supported values.
     public struct Supported {
         /// The version of `.xcscheme` files supported by Xcodeproj
         static let xcschemeFormatVersion = "1.3"
     }
-
+    
     /// Returns the Xcode file type for any given extension.
     ///
     /// - Parameter extension: file extension.
@@ -98,6 +98,6 @@ public struct Xcode {
             "xctest": "wrapper.cfbundle",
             "xib": "file.xib",
             "zip": "archive.zip",
-        ][`extension`]
+            ][`extension`]
     }
 }
