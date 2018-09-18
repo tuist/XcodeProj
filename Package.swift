5ba34b4dc91cf8271209a272f617f28b90725512
Pedro Piñera <pepibumur@gmail.com>
2018-09-18 17:58:00 +0200

    Downgrade AEXML version

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
+
 let package = Package(
     name: "xcodeproj",
     products: [
         .library(name: "xcodeproj", targets: ["xcodeproj"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.3.0")),
-        .package(url: "https://github.com/apple/swift-package-manager", .upToNextMajor(from: "0.2.0")),
+        .package(url: "https://github.com/tadija/AEXML.git", .exact("4.3.0")),
+        .package(url: "https://github.com/apple/swift-package-manager", .revision("ddfe6e23f3eaf3f79e003d872b6072a2686b4c88")),
     ],
     targets: [
         .target(name: "xcodeproj",
