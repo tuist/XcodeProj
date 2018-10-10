b87f4e3753847e62cb6d00da987d85dc0c7ee4d9
Yonas Kolb <yonaskolb@gmail.com>
2018-10-10 19:00:32 +1100

    encode PBXObject reference in target attributes

--- Tests/xcodeprojTests/Objects/Project/PBXProjectTests.swift
+++ Tests/xcodeprojTests/Objects/Project/PBXProjectTests.swift
@@ -5,7 +5,10 @@ final class PBXProjectTests: XCTestCase {
 
     func test_attributes() throws {
         let target = PBXTarget(name: "")
-        target.reference.fix("test")
+        target.reference.fix("app")
+
+        let testTarget = PBXTarget(name: "")
+        testTarget.reference.fix("test")
 
         let project = PBXProject(name: "",
                                  buildConfigurationList: XCConfigurationList(),
@@ -14,15 +17,16 @@ final class PBXProjectTests: XCTestCase {
                                  attributes: ["LastUpgradeCheck": "0940"],
                                  targetAttributes: [target: ["TestTargetID": "123"]])
 
-        project.setTargetAttributes(["custom": "abc"], target: target)
+        project.setTargetAttributes(["custom": "abc", "TestTargetID": testTarget], target: target)
 
         let plist = try project.plistKeyAndValue(proj: PBXProj(), reference: "")
         let attributes = plist.value.dictionary?["attributes"]?.dictionary ?? [:]
 
         let expectedAttributes: [CommentedString: PlistValue] = [
             "LastUpgradeCheck": "0940",
-            "TargetAttributes": ["test": [
+            "TargetAttributes": ["app": [
                 "custom": "abc",
+                "TestTargetID": "test"
             ]],
         ]
         XCTAssertEqual(attributes, expectedAttributes)
