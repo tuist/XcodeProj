eeb43546ce0741515f74b9a933846c65f183a99f
Sam Duke <samskiter@users.noreply.github.com>
2018-10-25 21:18:56 +0100

    Try to improve first-time-uniqueness of references. Simplify unique reference search.

--- Sources/xcodeproj/Utils/ReferenceGenerator.swift
+++ Sources/xcodeproj/Utils/ReferenceGenerator.swift
@@ -276,18 +276,22 @@ extension ReferenceGenerator {
                 identifiers.append(context)
             }
             let typeName = String(describing: type(of: object))
-            let id = ([typeName] + identifiers).joined(separator: "-").md5.uppercased()
-            
+
+            // Get acronym to be used as prefix for the reference.
+            // PBXFileReference is turned to FR.
+            let acronym = typeName
+                .replacingOccurrences(of: "PBX", with: "")
+                .replacingOccurrences(of: "XC", with: "")
+                .filter { String($0).lowercased() != String($0) }
+
+            let id = "\(acronym)_\(([typeName] + identifiers).joined(separator: "-"))"
+
             var reference = ""
             var counter = 0
-            // get the first reference that doesn't already exist
+            // Get the first reference that doesn't already exist
             repeat {
                 counter += 1
-                reference = id
-                if counter > 1 {
-                    reference += "_\(counter)"
-                    reference = reference.md5.uppercased()
-                }
+                reference = "\(id)_\(counter)".md5.uppercased()
             } while (references.contains(reference))
             references.insert(reference)
             object.reference.fix(reference)
