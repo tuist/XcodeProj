13ccbcd33a607d7c70264e28edc223eb0df8e53c
Pedro Piñera <pedro@ppinera.es>
2019-03-29 08:52:10 +0100

    Update CHANGELOG

--- Sources/main/main.swift
+++
@@ -1,8 +0,0 @@
-import xcodeproj
-import PathKit
-
-let path = Path("/Users/pedropinera/src/github.com/angledotdev/macos/Angle.xcodeproj")
-let project = try XcodeProj(path: path)
-try project.write(path: path)
-
-print("yolo")
