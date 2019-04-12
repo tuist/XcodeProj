5d47fe2c2ef099d9949bd2a07589d109365f3c1e
Damir Davletov <damir.davletov@corp.badoo.com>
2019-04-12 14:28:27 +0100

    Relative path calculation issue fix

---
+++ Tests/xcodeprojTests/Extensions/PathExtrasTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+import PathKit
+@testable import xcodeproj
+
+final class PathExtrasTests: XCTestCase {
+    func testThat_GivenAbsoluteSubPath_WhenRelativeToAbsoluteSuperPath_ThenResultIsTheRemainder() {
+        XCTAssertEqual(Path("/absolute/dir/file.txt").relative(to: Path("/absolute")), Path("dir/file.txt"))
+    }
+
+    func testThat_GivenAbsolutePath_WhenRelativeToNotSuperseedingAbsolutePath_ThenResultHasDoubleDot() {
+        XCTAssertEqual(Path("/absolute/dir/file.txt").relative(to: Path("/absolute/anotherDir")), Path("../dir/file.txt"))
+    }
+
+    func testThat_GivenSubPath_WhenRelativeToSuperPath_ThenResultIsTheRemainder() {
+        XCTAssertEqual(Path("some/dir/file.txt").relative(to: Path("some")), Path("dir/file.txt"))
+    }
+
+    func testThat_GivenPath_WhenRelativeToNotSuperseedingPath_ThenResultHasDoubleDot() {
+        XCTAssertEqual(Path("some/dir/file.txt").relative(to: Path("anotherDir")), Path("../some/dir/file.txt"))
+    }
+}
