7e6cf88d24ac15f5a1f7e173975d91106ddfa2dc
Kassem Wridan <kas@mxpr.net>
2019-06-07 20:52:08 +0100

    updating fixture such that it compiles
    - Swift 5 update
    - Updating script phase to `echo` so it can successfully compile

--- Fixtures/iOS/iOS/AppDelegate.swift
+++ Fixtures/iOS/iOS/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
