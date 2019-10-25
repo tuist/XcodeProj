2d0d5bb475e69c05fd4dcb8f18e5327af75b4c10
Marek Fořt <marek.fort@ackee.cz>
2019-10-25 16:16:42 +0200

    Edit RELEASE.md steps.

--- Tapestries/Sources/TapestryConfig/TapestryConfig.swift
+++ Tapestries/Sources/TapestryConfig/TapestryConfig.swift
@@ -1,11 +1,13 @@
 import PackageDescription
 
 let config = TapestryConfig(release: Release(actions: [.pre(.dependenciesCompatibility([.cocoapods, .spm(.all)])),
+                                                       .pre(tool: "bash", arguments: ["<(curl -Ls https://install.tuist.io)"]),
                                                        .pre(tool: "tuist", arguments: ["generate"]),
                                                        .pre(tool: "bundle", arguments: ["exec", "rake", "carthage_update_dependencies"]),
                                                        .pre(tool: "bundle", arguments: ["exec", "rake", "release_check"]),
                                                        .pre(.docsUpdate),
-                                                       .post(tool: "bundle", arguments: ["exec", "pod", "trunk", "push", "--allow-warnings", "--verbose"])],
+                                                       .post(tool: "bundle", arguments: ["exec", "pod", "trunk", "push", "--allow-warnings", "--verbose"]),
+                                                       .post(tool: "bundle", arguments: ["exec", "rake", "archive_carthage"])],
                                              add: ["README.md",
                                                    "xcodeproj.podspec",
                                                    "CHANGELOG.md"],
