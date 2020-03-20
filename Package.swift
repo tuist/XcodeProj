9d53df0781a897883515372d0babff62ef7713fa
Michael Eisel <michael.eisel@gmail.com>
2020-03-20 11:48:34 -0400

    update xcodegencext

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "XcodeProj", targets: ["XcodeProj"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tuist/XcodeProjCExt", .exact("7.8.0")),
+        .package(url: "https://github.com/tuist/XcodeProjCExt", .exact("7.8.0-1")),
         .package(url: "https://github.com/tadija/AEXML", .upToNextMinor(from: "4.4.0")),
         .package(url: "https://github.com/kylef/PathKit", .upToNextMinor(from: "1.0.0")),
     ],
