15512ee3624d830577c2f60840bc301b8c656b61
Artem Loenko <artyom.loenko@mac.com>
2021-02-15 13:15:11 +0000

    Update settingRegex regular expression with comments support

--- Sources/XcodeProj/Utils/XCConfig.swift
+++ Sources/XcodeProj/Utils/XCConfig.swift
@@ -99,7 +99,7 @@ final class XCConfigParser {
     // swiftlint:disable:next force_try
     private static var includeRegex = try! NSRegularExpression(pattern: "#include\\s+\"(.+\\.xcconfig)\"", options: .caseInsensitive)
     // swiftlint:disable:next force_try
-    private static var settingRegex = try! NSRegularExpression(pattern: "^([a-zA-Z0-9_\\[\\]=\\*~]+)\\s*=\\s*(\"?.*?\"?)\\s*(?:;\\s*)?$", options: [])
+    private static var settingRegex = try! NSRegularExpression(pattern: "^([a-zA-Z0-9_\\[\\]=\\*~]+)\\s*=\\s*(\"?.*?\"?)\\s*(?:;\\s*)?(?=$|\\/\\/)", options: [])
 }
 
 // MARK: - XCConfig Extension (Equatable)
