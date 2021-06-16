9ed873888cd3921e762356080c15ae80de0409b9
Kassem Wridan <kwridan@bloomberg.net>
2021-06-16 07:07:32 +0100

    Make `runPostActionsOnFailure` optional
    Resolves: https://github.com/tuist/tuist/issues/2991
    
    - Generated projects had `runPostActionsOnFailure` set in their schemes by default to `NO`
    - Xcode ends up removing this attribute when it's value is `NO` resultting in producing git diffs for any checked in projects that were previously generated
    - To mitigate this, the `runPostActionsOnFailure` option is being changed to an optional one where it's only written in the case it's explicitly defined to a specific value
    
    Test Plan:
    
    - Verify unit tests pass

--- Sources/XcodeProj/Scheme/XCScheme+BuildAction.swift
+++ Sources/XcodeProj/Scheme/XCScheme+BuildAction.swift
@@ -75,7 +75,7 @@ extension XCScheme {
         public var buildActionEntries: [Entry]
         public var parallelizeBuild: Bool
         public var buildImplicitDependencies: Bool
-        public var runPostActionsOnFailure: Bool
+        public var runPostActionsOnFailure: Bool?
 
         // MARK: - Init
 
@@ -84,7 +84,7 @@ extension XCScheme {
                     postActions: [ExecutionAction] = [],
                     parallelizeBuild: Bool = false,
                     buildImplicitDependencies: Bool = false,
-                    runPostActionsOnFailure: Bool = false) {
+                    runPostActionsOnFailure: Bool? = nil) {
             self.buildActionEntries = buildActionEntries
             self.parallelizeBuild = parallelizeBuild
             self.buildImplicitDependencies = buildImplicitDependencies
@@ -95,7 +95,7 @@ extension XCScheme {
         override init(element: AEXMLElement) throws {
             parallelizeBuild = element.attributes["parallelizeBuildables"].map { $0 == "YES" } ?? true
             buildImplicitDependencies = element.attributes["buildImplicitDependencies"].map { $0 == "YES" } ?? true
-            runPostActionsOnFailure = element.attributes["runPostActionsOnFailure"].map { $0 == "YES" } ?? false
+            runPostActionsOnFailure = element.attributes["runPostActionsOnFailure"].map { $0 == "YES" }
             buildActionEntries = try element["BuildActionEntries"]["BuildActionEntry"]
                 .all?
                 .map(Entry.init) ?? []
@@ -114,13 +114,18 @@ extension XCScheme {
         // MARK: - XML
 
         func xmlElement() -> AEXMLElement {
+            var attributes = [
+                "parallelizeBuildables": parallelizeBuild.xmlString,
+                "buildImplicitDependencies": buildImplicitDependencies.xmlString,
+            ]
+
+            if let runPostActionsOnFailure = runPostActionsOnFailure {
+                attributes["runPostActionsOnFailure"] = runPostActionsOnFailure.xmlString
+            }
+
             let element = AEXMLElement(name: "BuildAction",
                                        value: nil,
-                                       attributes: [
-                                           "parallelizeBuildables": parallelizeBuild.xmlString,
-                                           "buildImplicitDependencies": buildImplicitDependencies.xmlString,
-                                           "runPostActionsOnFailure": runPostActionsOnFailure.xmlString,
-                                       ])
+                                       attributes: attributes)
             super.writeXML(parent: element)
             let entries = element.addChild(name: "BuildActionEntries")
             buildActionEntries.forEach { entry in
