3a22f2f51e4317700870fcbee49bc65606d462b6
Pedro Piñera <pepibumur@gmail.com>
2021-09-17 17:28:49 +0200

    Update pacakges

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2.0
 
 import PackageDescription
 
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "XcodeProj", targets: ["XcodeProj"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tadija/AEXML", .upToNextMinor(from: "4.6.1")),
-        .package(url: "https://github.com/kylef/PathKit", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMinor(from: "4.6.1")),
+        .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "1.0.0")),
     ],
     targets: [
         .target(name: "XcodeProj",
