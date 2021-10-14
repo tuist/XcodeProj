d4b2a2aff6b360d3b4799fb58e2b0827d18624c9
Pedro Piñera <pepibumur@gmail.com>
2021-10-14 17:30:21 +0200

    Update the project to Tuist 2

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,6 +1,6 @@
 import ProjectDescription
 
-let config = TuistConfig(generationOptions: [
+let config = Config(generationOptions: [
     // If we generate the manifest target Carthage will attempt to compile it too.
     // .generateManifest
 ])
