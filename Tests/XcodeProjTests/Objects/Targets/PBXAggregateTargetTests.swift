4662c0155f62f769224c273cc73713e6e350346c
Brentley Jones <github@brentleyjones.com>
2022-04-08 20:02:46 -0500

    Add `addDependency()` helper method to `PBXAggregateTarget`

--- Tests/XcodeProjTests/Objects/Targets/PBXAggregateTargetTests.swift
+++ Tests/XcodeProjTests/Objects/Targets/PBXAggregateTargetTests.swift
@@ -27,4 +27,34 @@ final class PBXAggregateTargetTests: XCTestCase {
             "name": "name",
         ]
     }
+
+    func test_addDependency() throws {
+        let objects = PBXObjects(objects: [])
+
+        let configurationList = XCConfigurationList.fixture()
+        let mainGroup = PBXGroup.fixture()
+        objects.add(object: configurationList)
+        objects.add(object: mainGroup)
+
+        let project = PBXProject(name: "Project",
+                                 buildConfigurationList: configurationList,
+                                 compatibilityVersion: "0",
+                                 mainGroup: mainGroup)
+
+        objects.add(object: project)
+        let target = PBXAggregateTarget(name: "Target", buildConfigurationList: nil)
+        let dependency = PBXAggregateTarget(name: "Dependency", buildConfigurationList: nil)
+        objects.add(object: target)
+        objects.add(object: dependency)
+        _ = try target.addDependency(target: dependency)
+        let targetDependency: PBXTargetDependency? = target.dependencyReferences.first?.getObject()
+
+        XCTAssertEqual(targetDependency?.name, "Dependency")
+        XCTAssertEqual(targetDependency?.targetReference, dependency.reference)
+        let containerItemProxy: PBXContainerItemProxy? = targetDependency?.targetProxyReference?.getObject()
+        XCTAssertEqual(containerItemProxy?.containerPortalReference, project.reference)
+        XCTAssertEqual(containerItemProxy?.remoteGlobalID?.uuid, dependency.reference.value)
+        XCTAssertEqual(containerItemProxy?.proxyType, .nativeTarget)
+        XCTAssertEqual(containerItemProxy?.remoteInfo, "Dependency")
+    }
 }
