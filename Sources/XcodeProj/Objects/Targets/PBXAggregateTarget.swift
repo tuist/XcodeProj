077da3f0307c4d24a721ed4f7137a75fa443b549
Brentley Jones <github@brentleyjones.com>
2022-04-27 15:03:16 -0500

    Add `addDependency()` helper method to `PBXAggregateTarget` (#677)

--- Sources/XcodeProj/Objects/Targets/PBXAggregateTarget.swift
+++ Sources/XcodeProj/Objects/Targets/PBXAggregateTarget.swift
@@ -15,3 +15,30 @@ extension PBXAggregateTarget: PlistSerializable {
         return try plistValues(proj: proj, isa: PBXAggregateTarget.isa, reference: reference)
     }
 }
+
+// MARK: - Helpers
+
+public extension PBXAggregateTarget {
+    /// Adds a local target dependency to the target.
+    ///
+    /// - Parameter target: dependency target.
+    /// - Returns: target dependency reference.
+    /// - Throws: an error if the dependency cannot be created.
+    func addDependency(target: PBXTarget) throws -> PBXTargetDependency? {
+        let objects = try target.objects()
+        guard let project = objects.projects.first?.value else {
+            return nil
+        }
+        let proxy = PBXContainerItemProxy(containerPortal: .project(project),
+                                          remoteGlobalID: .object(target),
+                                          proxyType: .nativeTarget,
+                                          remoteInfo: target.name)
+        objects.add(object: proxy)
+        let targetDependency = PBXTargetDependency(name: target.name,
+                                                   target: target,
+                                                   targetProxy: proxy)
+        objects.add(object: targetDependency)
+        dependencies.append(targetDependency)
+        return targetDependency
+    }
+}
