19fd6c4d147624f52dd6b9c3c320ef31e4fcd850
Kassem Wridan <kwridan@bloomberg.net>
2022-07-14 08:24:25 +0100

    Fix Tuist generated project (for release)
    - Following `RELEASE.md` one of the steps is genreate a tuist project used for the Carthage integration
    - The Tuist config was out dated (generationOptions API has changed in Tuist 3.x)
    - The steps ordering was incorrect, to ensure a successful generation, the carthage dependency frameworks need to be built first
    
    Test Plan:
    
    - Run `bundle exec rake carthage_update_dependencies`
    - Run `tuist generate`
    - Verify the project successfully generates

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,6 +1,3 @@
 import ProjectDescription
 
-let config = Config(generationOptions: [
-    // If we generate the manifest target Carthage will attempt to compile it too.
-    // .generateManifest
-])
+let config = Config(generationOptions: .options())
