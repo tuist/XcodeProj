2a9df9b614e1fe125f0ad471e13659e99dbf40ec
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2022-12-01 09:37:38 -0500

    Set `platformFilter` in `PBXBuildFile`
    This attribute was not being set

--- Tests/XcodeProjTests/Objects/BuildPhase/PBXBuildFileTests.swift
+++ Tests/XcodeProjTests/Objects/BuildPhase/PBXBuildFileTests.swift
@@ -6,4 +6,11 @@ final class PBXBuildFileTests: XCTestCase {
     func test_isa_returnsTheCorrectValue() {
         XCTAssertEqual(PBXBuildFile.isa, "PBXBuildFile")
     }
+
+    func test_platformFilterIsSet() {
+        let pbxBuildFile: PBXBuildFile = PBXBuildFile(
+            platformFilter: "platformFilter"
+        )
+        XCTAssertEqual(pbxBuildFile.platformFilter, "platformFilter")
+    }
 }
