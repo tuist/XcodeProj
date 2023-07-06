0166ff17b7490aa735772229028168530bd04797
Pedro Piñera <pedro@pepicrft.me>
2023-07-06 15:54:27 +0200

    Add missing files

---
+++ Fixtures/TargetWithCustomBuildRules/TargetWithCustomBuildRules/TargetWithCustomBuildRules.m
@@ -0,0 +1,12 @@
+//
+//  TargetWithCustomBuildRules.m
+//  TargetWithCustomBuildRules
+//
+//  Created by Pedro Piñera Buendía on 06.07.23.
+//
+
+#import "TargetWithCustomBuildRules.h"
+
+@implementation TargetWithCustomBuildRules
+
+@end
