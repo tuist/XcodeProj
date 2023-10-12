b6a082add239529a2ad350027246f5b203036a84
Ruslan Alikhamov <r.alikhamov@gmail.com>
2023-10-13 03:15:31 +0400

    Exposed two new fixtures

--- Tests/XcodeProjTests/Tests/Fixtures.swift
+++ Tests/XcodeProjTests/Tests/Fixtures.swift
@@ -26,3 +26,13 @@ func iosProjectWithXCLocalSwiftPackageReference() -> (Path, [String: Any]) {
     let iosProjectWithXCLocalSwiftPackageReference = fixturesPath() + "iOS/ProjectWithXCLocalSwiftPackageReference.xcodeproj/project.pbxproj"
     return (iosProjectWithXCLocalSwiftPackageReference, loadPlist(path: iosProjectWithXCLocalSwiftPackageReference.string)!)
 }
+
+func iosProjectWithRelativeXCLocalSwiftPackageReferences() -> (Path, [String: Any]) {
+    let iosProjectWithXCLocalSwiftPackageReference = fixturesPath() + "iOS/ProjectWithRelativeXCLocalSwiftPackageReference/ProjectWithRelativeXCLocalSwiftPackageReference.xcodeproj/project.pbxproj"
+    return (iosProjectWithXCLocalSwiftPackageReference, loadPlist(path: iosProjectWithXCLocalSwiftPackageReference.string)!)
+}
+
+func iosProjectWithXCLocalSwiftPackageReferences() -> (Path, [String: Any]) {
+    let iosProjectWithXCLocalSwiftPackageReference = fixturesPath() + "iOS/ProjectWithXCLocalSwiftPackageReferences.xcodeproj/project.pbxproj"
+    return (iosProjectWithXCLocalSwiftPackageReference, loadPlist(path: iosProjectWithXCLocalSwiftPackageReference.string)!)
+}
