dc0187ca3d0e545d205be521aa47eb50c9f25f20
Mike Simons <msimons@etsy.com>
2024-07-20 16:12:09 -0400

    Migrate `BuildSettings` to use `BuildSetting` enum.

--- Tests/XcodeProjTests/Utils/BuildSettingsProviderTests.swift
+++ Tests/XcodeProjTests/Utils/BuildSettingsProviderTests.swift
@@ -9,21 +9,22 @@ class BuildSettingProviderTests: XCTestCase {
                                                           platform: .iOS,
                                                           product: .application,
                                                           swift: true)
-
+      let expected: BuildSettings = [
+        "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
+        "ENABLE_PREVIEWS": "YES",
+        "CODE_SIGN_IDENTITY": "iPhone Developer",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/Frameworks",
+        ],
+        "SDKROOT": "iphoneos",
+        "SWIFT_COMPILATION_MODE": "wholemodule",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
+        "TARGETED_DEVICE_FAMILY": "1,2",
+    ]
+      
         // Then
-        assertEqualSettings(results, [
-            "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
-            "ENABLE_PREVIEWS": "YES",
-            "CODE_SIGN_IDENTITY": "iPhone Developer",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/Frameworks",
-            ],
-            "SDKROOT": "iphoneos",
-            "SWIFT_COMPILATION_MODE": "wholemodule",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
-            "TARGETED_DEVICE_FAMILY": "1,2",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_iosFramework() {
@@ -32,30 +33,31 @@ class BuildSettingProviderTests: XCTestCase {
                                                           platform: .iOS,
                                                           product: .framework,
                                                           swift: true)
-
+      let expected: BuildSettings = [
+        "CODE_SIGN_IDENTITY": "",
+        "CURRENT_PROJECT_VERSION": "1",
+        "DEFINES_MODULE": "YES",
+        "DYLIB_COMPATIBILITY_VERSION": "1",
+        "DYLIB_CURRENT_VERSION": "1",
+        "DYLIB_INSTALL_NAME_BASE": "@rpath",
+        "INSTALL_PATH": "$(LOCAL_LIBRARY_DIR)/Frameworks",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/Frameworks",
+            "@loader_path/Frameworks",
+        ],
+        "PRODUCT_NAME": "$(TARGET_NAME:c99extidentifier)",
+        "SDKROOT": "iphoneos",
+        "SKIP_INSTALL": "YES",
+        "SWIFT_COMPILATION_MODE": "wholemodule",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
+        "TARGETED_DEVICE_FAMILY": "1,2",
+        "VERSIONING_SYSTEM": "apple-generic",
+        "VERSION_INFO_PREFIX": "",
+    ]
+      
         // Then
-        assertEqualSettings(results, [
-            "CODE_SIGN_IDENTITY": "",
-            "CURRENT_PROJECT_VERSION": "1",
-            "DEFINES_MODULE": "YES",
-            "DYLIB_COMPATIBILITY_VERSION": "1",
-            "DYLIB_CURRENT_VERSION": "1",
-            "DYLIB_INSTALL_NAME_BASE": "@rpath",
-            "INSTALL_PATH": "$(LOCAL_LIBRARY_DIR)/Frameworks",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/Frameworks",
-                "@loader_path/Frameworks",
-            ],
-            "PRODUCT_NAME": "$(TARGET_NAME:c99extidentifier)",
-            "SDKROOT": "iphoneos",
-            "SKIP_INSTALL": "YES",
-            "SWIFT_COMPILATION_MODE": "wholemodule",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
-            "TARGETED_DEVICE_FAMILY": "1,2",
-            "VERSIONING_SYSTEM": "apple-generic",
-            "VERSION_INFO_PREFIX": "",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_iosExtension() {
@@ -64,20 +66,20 @@ class BuildSettingProviderTests: XCTestCase {
                                                           platform: .iOS,
                                                           product: .appExtension,
                                                           swift: true)
-
+      let expected: BuildSettings = [
+        "CODE_SIGN_IDENTITY": "iPhone Developer",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/Frameworks",
+            "@executable_path/../../Frameworks",
+        ],
+        "SDKROOT": "iphoneos",
+        "SWIFT_COMPILATION_MODE": "wholemodule",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
+        "TARGETED_DEVICE_FAMILY": "1,2",
+    ]
         // Then
-        assertEqualSettings(results, [
-            "CODE_SIGN_IDENTITY": "iPhone Developer",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/Frameworks",
-                "@executable_path/../../Frameworks",
-            ],
-            "SDKROOT": "iphoneos",
-            "SWIFT_COMPILATION_MODE": "wholemodule",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
-            "TARGETED_DEVICE_FAMILY": "1,2",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_macOSAplication() {
@@ -87,20 +89,21 @@ class BuildSettingProviderTests: XCTestCase {
                                                           product: .application,
                                                           swift: true)
 
+      let expected: BuildSettings = [
+        "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
+        "ENABLE_PREVIEWS": "YES",
+        "CODE_SIGN_IDENTITY": "-",
+        "COMBINE_HIDPI_IMAGES": "YES",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/../Frameworks",
+        ],
+        "SDKROOT": "macosx",
+        "SWIFT_COMPILATION_MODE": "wholemodule",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
+    ]
         // Then
-        assertEqualSettings(results, [
-            "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
-            "ENABLE_PREVIEWS": "YES",
-            "CODE_SIGN_IDENTITY": "-",
-            "COMBINE_HIDPI_IMAGES": "YES",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/../Frameworks",
-            ],
-            "SDKROOT": "macosx",
-            "SWIFT_COMPILATION_MODE": "wholemodule",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_tvOSAplication() {
@@ -110,20 +113,22 @@ class BuildSettingProviderTests: XCTestCase {
                                                           product: .application,
                                                           swift: true)
 
+      let expected: BuildSettings = [
+        "ASSETCATALOG_COMPILER_APPICON_NAME": "App Icon & Top Shelf Image",
+        "ENABLE_PREVIEWS": "YES",
+        "ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME": "LaunchImage",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/Frameworks",
+        ],
+        "SDKROOT": "appletvos",
+        "SWIFT_COMPILATION_MODE": "wholemodule",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
+        "TARGETED_DEVICE_FAMILY": "3",
+    ]
+      
         // Then
-        assertEqualSettings(results, [
-            "ASSETCATALOG_COMPILER_APPICON_NAME": "App Icon & Top Shelf Image",
-            "ENABLE_PREVIEWS": "YES",
-            "ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME": "LaunchImage",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/Frameworks",
-            ],
-            "SDKROOT": "appletvos",
-            "SWIFT_COMPILATION_MODE": "wholemodule",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
-            "TARGETED_DEVICE_FAMILY": "3",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_watchOSAplication() {
@@ -132,9 +137,8 @@ class BuildSettingProviderTests: XCTestCase {
                                                           platform: .watchOS,
                                                           product: .application,
                                                           swift: true)
-
-        // Then
-        assertEqualSettings(results, [
+        
+        let expected: BuildSettings = [
             "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
             "ENABLE_PREVIEWS": "YES",
             "LD_RUNPATH_SEARCH_PATHS": ["$(inherited)", "@executable_path/Frameworks"],
@@ -143,9 +147,12 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_COMPILATION_MODE": "wholemodule",
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "4",
-        ])
+        ]
+        
+        XCTAssertEqual(results, expected)
+        
     }
-
+    
     func test_targetSettings_watchOSFramework() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
@@ -154,7 +161,7 @@ class BuildSettingProviderTests: XCTestCase {
                                                           swift: true)
 
         // Then
-        assertEqualSettings(results, [
+        let expected: BuildSettings = [
             "APPLICATION_EXTENSION_API_ONLY": "YES",
             "CODE_SIGN_IDENTITY": "",
             "CURRENT_PROJECT_VERSION": "1",
@@ -176,7 +183,10 @@ class BuildSettingProviderTests: XCTestCase {
             "TARGETED_DEVICE_FAMILY": "4",
             "VERSIONING_SYSTEM": "apple-generic",
             "VERSION_INFO_PREFIX": "",
-        ])
+        ]
+        
+        XCTAssertEqual(results, expected)
+
     }
 
     func test_targetSettings_watchOSExtension() {
@@ -187,7 +197,7 @@ class BuildSettingProviderTests: XCTestCase {
                                                           swift: true)
 
         // Then
-        assertEqualSettings(results, [
+        let expected: BuildSettings = [
             "LD_RUNPATH_SEARCH_PATHS": [
                 "$(inherited)",
                 "@executable_path/Frameworks",
@@ -198,7 +208,9 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_COMPILATION_MODE": "wholemodule",
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "4",
-        ])
+        ]
+
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_visionOSAplication() {
@@ -209,7 +221,7 @@ class BuildSettingProviderTests: XCTestCase {
                                                           swift: true)
 
         // Then
-        assertEqualSettings(results, [
+        let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "iPhone Developer",
             "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
             "ENABLE_PREVIEWS": "YES",
@@ -221,7 +233,9 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_COMPILATION_MODE": "wholemodule",
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "1,2,7",
-        ])
+        ]
+        
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_visionOSFramework() {
@@ -232,7 +246,7 @@ class BuildSettingProviderTests: XCTestCase {
                                                           swift: true)
 
         // Then
-        assertEqualSettings(results, [
+        let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "",
             "CURRENT_PROJECT_VERSION": "1",
             "DEFINES_MODULE": "YES",
@@ -253,7 +267,9 @@ class BuildSettingProviderTests: XCTestCase {
             "TARGETED_DEVICE_FAMILY": "1,2,7",
             "VERSIONING_SYSTEM": "apple-generic",
             "VERSION_INFO_PREFIX": "",
-        ])
+        ]
+        
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_visionOSExtension() {
@@ -264,7 +280,7 @@ class BuildSettingProviderTests: XCTestCase {
                                                           swift: true)
 
         // Then
-        assertEqualSettings(results, [
+        let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "iPhone Developer",
             "LD_RUNPATH_SEARCH_PATHS": [
                 "$(inherited)",
@@ -275,7 +291,9 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_COMPILATION_MODE": "wholemodule",
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "1,2,7",
-        ])
+        ]
+        
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_iOSUnitTests() {
@@ -285,20 +303,22 @@ class BuildSettingProviderTests: XCTestCase {
                                                           product: .unitTests,
                                                           swift: true)
 
+      let expected: BuildSettings = [
+        "CODE_SIGN_IDENTITY": "iPhone Developer",
+        "SDKROOT": "iphoneos",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/Frameworks",
+            "@loader_path/Frameworks",
+        ],
+        "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
+        "SWIFT_COMPILATION_MODE": "singlefile",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
+        "TARGETED_DEVICE_FAMILY": "1,2",
+    ]
+      
         // Then
-        assertEqualSettings(results, [
-            "CODE_SIGN_IDENTITY": "iPhone Developer",
-            "SDKROOT": "iphoneos",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/Frameworks",
-                "@loader_path/Frameworks",
-            ],
-            "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
-            "SWIFT_COMPILATION_MODE": "singlefile",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
-            "TARGETED_DEVICE_FAMILY": "1,2",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_iOSUITests() {
@@ -307,21 +327,23 @@ class BuildSettingProviderTests: XCTestCase {
                                                           platform: .iOS,
                                                           product: .uiTests,
                                                           swift: true)
-
+      let expected: BuildSettings =  [
+        "CODE_SIGN_IDENTITY": "iPhone Developer",
+        "SDKROOT": "iphoneos",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/Frameworks",
+            "@loader_path/Frameworks",
+        ],
+        "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
+        "SWIFT_COMPILATION_MODE": "singlefile",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
+        "TARGETED_DEVICE_FAMILY": "1,2",
+    ]
+      
+      
         // Then
-        assertEqualSettings(results, [
-            "CODE_SIGN_IDENTITY": "iPhone Developer",
-            "SDKROOT": "iphoneos",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/Frameworks",
-                "@loader_path/Frameworks",
-            ],
-            "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
-            "SWIFT_COMPILATION_MODE": "singlefile",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
-            "TARGETED_DEVICE_FAMILY": "1,2",
-        ])
+        XCTAssertEqual(results, expected )
     }
 
     func test_targetSettings_macOSUnitTests() {
@@ -331,19 +353,21 @@ class BuildSettingProviderTests: XCTestCase {
                                                           product: .unitTests,
                                                           swift: true)
 
+      let expected: BuildSettings =  [
+        "CODE_SIGN_IDENTITY": "-",
+        "SDKROOT": "macosx",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/../Frameworks",
+            "@loader_path/../Frameworks",
+        ],
+        "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
+        "SWIFT_COMPILATION_MODE": "singlefile",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
+    ]
+      
         // Then
-        assertEqualSettings(results, [
-            "CODE_SIGN_IDENTITY": "-",
-            "SDKROOT": "macosx",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/../Frameworks",
-                "@loader_path/../Frameworks",
-            ],
-            "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
-            "SWIFT_COMPILATION_MODE": "singlefile",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_tvOSUnitTests() {
@@ -352,20 +376,21 @@ class BuildSettingProviderTests: XCTestCase {
                                                           platform: .tvOS,
                                                           product: .unitTests,
                                                           swift: true)
+      let expected: BuildSettings = [
+        "SDKROOT": "appletvos",
+        "LD_RUNPATH_SEARCH_PATHS": [
+            "$(inherited)",
+            "@executable_path/Frameworks",
+            "@loader_path/Frameworks",
+        ],
+        "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
+        "SWIFT_COMPILATION_MODE": "singlefile",
+        "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
+        "TARGETED_DEVICE_FAMILY": "3",
+    ]
 
         // Then
-        assertEqualSettings(results, [
-            "SDKROOT": "appletvos",
-            "LD_RUNPATH_SEARCH_PATHS": [
-                "$(inherited)",
-                "@executable_path/Frameworks",
-                "@loader_path/Frameworks",
-            ],
-            "SWIFT_ACTIVE_COMPILATION_CONDITIONS": ["$(inherited)", "DEBUG"],
-            "SWIFT_COMPILATION_MODE": "singlefile",
-            "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
-            "TARGETED_DEVICE_FAMILY": "3",
-        ])
+        XCTAssertEqual(results, expected)
     }
 
     func test_targetSettings_visionOSUnitTests() {
@@ -416,10 +441,10 @@ class BuildSettingProviderTests: XCTestCase {
 
     // MARK: - Helpers
 
-    func assertEqualSettings(_ lhs: BuildSettings, _ rhs: BuildSettings, file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(lhs as NSDictionary,
-                       rhs as NSDictionary,
-                       file: file,
-                       line: line)
-    }
+//    func XCTAssertEqual(_ lhs: BuildSettings, _ rhs: BuildSettings, file: StaticString = #file, line: UInt = #line) {
+//        XCTAssertEqual(lhs as NSDictionary,
+//                       rhs as NSDictionary,
+//                       file: file,
+//                       line: line)
+//    }
 }
