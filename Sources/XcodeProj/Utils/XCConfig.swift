bf6a64af9b4d2f6b1bdea66b6fd48c5a28253e49
Mike Simons <msimons@etsy.com>
2024-07-22 20:13:33 -0400

    Fix Tests

--- Sources/XcodeProj/Utils/XCConfig.swift
+++ Sources/XcodeProj/Utils/XCConfig.swift
@@ -124,8 +124,8 @@ extension XCConfig {
     /// It returns the build settings after flattening all the includes.
     ///
     /// - Returns: build settings flattening all the includes.
-    public func flattenedBuildSettings() -> [String: Any] {
-        var content: [String: Any] = buildSettings
+    public func flattenedBuildSettings() -> [String: BuildSetting] {
+        var content: [String: BuildSetting] = buildSettings
         includes
             .map { $0.1 }
             .flattened()
