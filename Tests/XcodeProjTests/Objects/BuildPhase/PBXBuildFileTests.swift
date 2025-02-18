895b43a3ed0e957d90dff1ab7692097990ec4438
Mike Simons <msimons@etsy.com>
2025-02-18 09:14:25 -0500

    Add tests

--- Tests/XcodeProjTests/Objects/BuildPhase/PBXBuildFileTests.swift
+++ Tests/XcodeProjTests/Objects/BuildPhase/PBXBuildFileTests.swift
@@ -13,4 +13,22 @@ final class PBXBuildFileTests: XCTestCase {
         )
         XCTAssertEqual(pbxBuildFile.platformFilter, "platformFilter")
     }
+    
+    func test_platformCompilerFlagsIsSet() {
+        let expected = "flagValue"
+        let pbxBuildFile = PBXBuildFile(
+            settings: ["COMPILER_FLAGS": .string(expected)]
+        )
+        XCTAssertEqual(pbxBuildFile.compilerFlags, expected)
+    }
+    
+    func test_platformAttributesIsSet() {
+        let expected = ["Public"]
+        let pbxBuildFile = PBXBuildFile(
+            settings: ["ATTRIBUTES": .array(expected)]
+        )
+        XCTAssertEqual(pbxBuildFile.attributes, expected)
+    }
+    
+    
 }
