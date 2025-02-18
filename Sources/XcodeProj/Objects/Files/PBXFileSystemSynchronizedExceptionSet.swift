8eace7cf5249a6c37ca85ad637fc1ecc45efd0f6
fortmarek <marekfort@me.com>
2025-02-18 11:46:03 +0100

    Add comment

--- Sources/XcodeProj/Objects/Files/PBXFileSystemSynchronizedExceptionSet.swift
+++ Sources/XcodeProj/Objects/Files/PBXFileSystemSynchronizedExceptionSet.swift
@@ -1,3 +1,4 @@
 import Foundation
 
+/// Common class for exception sets, such as `PBXFileSystemSynchronizedGroupBuildPhaseMembershipExceptionSet` and `PBXFileSystemSynchronizedBuildFileExceptionSet`
 public class PBXFileSystemSynchronizedExceptionSet: PBXObject {}
