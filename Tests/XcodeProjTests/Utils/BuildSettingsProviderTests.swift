d56c55a4be201c89fd42a0fcbd0285c32a40e132
Mike Simons <msimons@etsy.com>
2025-02-27 12:13:43 -0500

    Delete commented code

--- Tests/XcodeProjTests/Utils/BuildSettingsProviderTests.swift
+++ Tests/XcodeProjTests/Utils/BuildSettingsProviderTests.swift
@@ -22,11 +22,11 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "1,2",
         ]
-
+        
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_iosFramework() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
@@ -55,11 +55,11 @@ class BuildSettingProviderTests: XCTestCase {
             "VERSIONING_SYSTEM": "apple-generic",
             "VERSION_INFO_PREFIX": "",
         ]
-
+        
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_iosExtension() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
@@ -81,14 +81,14 @@ class BuildSettingProviderTests: XCTestCase {
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_macOSAplication() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .macOS,
                                                           product: .application,
                                                           swift: true)
-
+        
         let expected: BuildSettings = [
             "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
             "ENABLE_PREVIEWS": "YES",
@@ -105,14 +105,14 @@ class BuildSettingProviderTests: XCTestCase {
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_tvOSAplication() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .tvOS,
                                                           product: .application,
                                                           swift: true)
-
+        
         let expected: BuildSettings = [
             "ASSETCATALOG_COMPILER_APPICON_NAME": "App Icon & Top Shelf Image",
             "ENABLE_PREVIEWS": "YES",
@@ -126,18 +126,18 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "3",
         ]
-
+        
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_watchOSAplication() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .watchOS,
                                                           product: .application,
                                                           swift: true)
-
+        
         let expected: BuildSettings = [
             "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
             "ENABLE_PREVIEWS": "YES",
@@ -148,17 +148,17 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "4",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_watchOSFramework() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .watchOS,
                                                           product: .framework,
                                                           swift: true)
-
+        
         // Then
         let expected: BuildSettings = [
             "APPLICATION_EXTENSION_API_ONLY": "YES",
@@ -183,17 +183,17 @@ class BuildSettingProviderTests: XCTestCase {
             "VERSIONING_SYSTEM": "apple-generic",
             "VERSION_INFO_PREFIX": "",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_watchOSExtension() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .watchOS,
                                                           product: .appExtension,
                                                           swift: true)
-
+        
         // Then
         let expected: BuildSettings = [
             "LD_RUNPATH_SEARCH_PATHS": [
@@ -207,17 +207,17 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "4",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_visionOSAplication() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .visionOS,
                                                           product: .application,
                                                           swift: true)
-
+        
         // Then
         let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "iPhone Developer",
@@ -232,17 +232,17 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "1,2,7",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_visionOSFramework() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .visionOS,
                                                           product: .framework,
                                                           swift: true)
-
+        
         // Then
         let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "",
@@ -266,17 +266,17 @@ class BuildSettingProviderTests: XCTestCase {
             "VERSIONING_SYSTEM": "apple-generic",
             "VERSION_INFO_PREFIX": "",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_visionOSExtension() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .release,
                                                           platform: .visionOS,
                                                           product: .appExtension,
                                                           swift: true)
-
+        
         // Then
         let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "iPhone Developer",
@@ -290,17 +290,17 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Owholemodule",
             "TARGETED_DEVICE_FAMILY": "1,2,7",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_iOSUnitTests() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .debug,
                                                           platform: .iOS,
                                                           product: .unitTests,
                                                           swift: true)
-
+        
         let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "iPhone Developer",
             "SDKROOT": "iphoneos",
@@ -314,11 +314,11 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
             "TARGETED_DEVICE_FAMILY": "1,2",
         ]
-
+        
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_iOSUITests() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .debug,
@@ -338,18 +338,18 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
             "TARGETED_DEVICE_FAMILY": "1,2",
         ]
-
+        
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_macOSUnitTests() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .debug,
                                                           platform: .macOS,
                                                           product: .unitTests,
                                                           swift: true)
-
+        
         let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "-",
             "SDKROOT": "macosx",
@@ -362,11 +362,11 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_COMPILATION_MODE": "singlefile",
             "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
         ]
-
+        
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_tvOSUnitTests() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .debug,
@@ -385,18 +385,18 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
             "TARGETED_DEVICE_FAMILY": "3",
         ]
-
+        
         // Then
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_visionOSUnitTests() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .debug,
                                                           platform: .visionOS,
                                                           product: .unitTests,
                                                           swift: true)
-
+        
         // Then
         let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "iPhone Developer",
@@ -411,17 +411,17 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
             "TARGETED_DEVICE_FAMILY": "1,2,7",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
+    
     func test_targetSettings_visionOSUITests() {
         // Given / When
         let results = BuildSettingsProvider.targetDefault(variant: .debug,
                                                           platform: .visionOS,
                                                           product: .uiTests,
                                                           swift: true)
-
+        
         // Then
         let expected: BuildSettings = [
             "CODE_SIGN_IDENTITY": "iPhone Developer",
@@ -436,16 +436,7 @@ class BuildSettingProviderTests: XCTestCase {
             "SWIFT_OPTIMIZATION_LEVEL": "-Onone",
             "TARGETED_DEVICE_FAMILY": "1,2,7",
         ]
-
+        
         XCTAssertEqual(results, expected)
     }
-
-    // MARK: - Helpers
-
-//    func XCTAssertEqual(_ lhs: BuildSettings, _ rhs: BuildSettings, file: StaticString = #file, line: UInt = #line) {
-//        XCTAssertEqual(lhs as NSDictionary,
-//                       rhs as NSDictionary,
-//                       file: file,
-//                       line: line)
-//    }
 }
