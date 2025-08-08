a6daf9a798b8099b17bdec0f8197583018ca0327
ToMark1881 <vlad182ava@gmail.com>
2025-08-08 15:04:38 +0300

    feat: possibility to get child group using path (#959)
    * added feature to get child group using path
    
    * unit tests for group(with path), iosProjectWithExtensionsData fixture
    
    * lint cleanup, path tests in separate file with #if
    
    * lint code style updates
    
    * code spacing lint update

--- Sources/XcodeProj/Objects/Files/PBXGroup.swift
+++ Sources/XcodeProj/Objects/Files/PBXGroup.swift
@@ -116,6 +116,16 @@ public extension PBXGroup {
             .first(where: { $0.name == name })
     }
 
+    /// Returns group with the given path contained in the given parent group.
+    ///
+    /// - Parameter path: path name.
+    /// - Returns: group with the given path contained in the given parent group.
+    func group(with path: String) -> PBXGroup? {
+        childrenReferences
+            .objects()
+            .first(where: { ($0 as? PBXFileElement)?.path == path }) as? PBXGroup
+    }
+
     /// Returns the synchronized root group with the given name contained in the given parent group.
     ///
     /// - Parameter groupName: group name.
